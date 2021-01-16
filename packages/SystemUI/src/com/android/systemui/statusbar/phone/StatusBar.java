9387a4190961dfd4c2c1c93dbbfa9558efcecd30
spezi77 <spezi7713@gmx.net>
2021-01-16 20:31:43 +0100

    Ambient music ticker: show Now Playing info on lockscreen
    abc: keep just the nowplaying part from PixelDust @spezi77 code
    and some code cleanup
    
    Change-Id: I51fcf831451788b758bfe59c05d424a44f444315
    
    ---
    
    for the ambient display, abc music ticker code already allows to show
    both local player and Now Playing info under the clock.
    
    In lockscreen, instead, the local player shows its own media
    notification, but Now Playing info are missing.
    
    This commit adds Now Playing to lockscreen so we can cover all cases.
    
    Change-Id: I2fb5746410dd6fc6352687a3cd29a3cda6577dd0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3878,6 +3878,14 @@ public class StatusBar extends SystemUI implements DemoMode,
         mNotificationPanelViewController.setDozing(mDozing, animate, mWakeUpTouchLocation);
         mVisualizerView.setDozing(mDozing);
         updateQsExpansionEnabled();
+
+        if (mAmbientIndicationContainer != null) {
+            ((AmbientIndicationContainer)mAmbientIndicationContainer)
+                    .updateDozingState(mDozing);
+        } else {
+            Log.d("StatusBar", "updateDozingState -> AmbientIndicationContainer null");
+        }
+
         Trace.endSection();
     }
 
@@ -3996,9 +4004,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mStatusBarStateController.fromShadeLocked());
             }
             if (mStatusBarView != null) mStatusBarView.removePendingHideExpandedRunnables();
-            if (mAmbientIndicationContainer != null) {
-                mAmbientIndicationContainer.setVisibility(View.VISIBLE);
-            }
         } else {
             mKeyguardIndicationController.setVisible(false);
             if (mKeyguardUserSwitcher != null) {
@@ -4007,9 +4012,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                                 mState == StatusBarState.SHADE_LOCKED ||
                                 mStatusBarStateController.fromShadeLocked());
             }
-            if (mAmbientIndicationContainer != null) {
-                mAmbientIndicationContainer.setVisibility(View.INVISIBLE);
-            }
         }
         updateDozingState();
         checkBarModes();
@@ -4017,6 +4019,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
         mVisualizerView.setStatusBarState(newState);
         updateKeyguardState();
+
+        if (mAmbientIndicationContainer != null) {
+            ((AmbientIndicationContainer)mAmbientIndicationContainer)
+                    .updateKeyguardState(mState == StatusBarState.KEYGUARD);
+        } else {
+            Log.d("StatusBar", "updateKeyguardState -> AmbientIndicationContainer null");
+        }
         Trace.endSection();
     }
 
