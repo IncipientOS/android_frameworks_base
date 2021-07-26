83d864959de0dd287284c1076931ce1206014696
ROM-Jeremy <anthony.asher1986@gmail.com>
2021-07-26 11:30:50 -0400

    Revert "Ambient music ticker: double tap on track title to skip track"
    This reverts commit 7aa3c3deda797fc4c675bf8bca2b53aad9edc40e.

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -35,7 +35,6 @@ public class AmbientDisplayConfiguration {
 
     private final Context mContext;
     private final boolean mAlwaysOnByDefault;
-    private final boolean mDeviceHasSoli;
     private final boolean mDozeEnabledByDefault;
 
     /** {@hide} */
@@ -43,7 +42,6 @@ public class AmbientDisplayConfiguration {
     public AmbientDisplayConfiguration(Context context) {
         mContext = context;
         mAlwaysOnByDefault = mContext.getResources().getBoolean(R.bool.config_dozeAlwaysOnEnabled);
-        mDeviceHasSoli = mContext.getResources().getBoolean(R.bool.config_has_Soli);
         mDozeEnabledByDefault = mContext.getResources().getBoolean(R.bool.config_doze_enabled_by_default);
     }
 
@@ -228,10 +226,5 @@ public class AmbientDisplayConfiguration {
     public boolean isAmbientTickerEnabled(int user) {
         return Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
-    }
-
-    /** {@hide} */
-    public boolean deviceHasSoli() {
-        return mDeviceHasSoli;
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -700,10 +700,6 @@ public class NotificationPanelViewController extends PanelViewController {
         return mKeyguardStatusView.hasCustomClock();
     }
 
-    public KeyguardStatusView getKeyguardStatusView() {
-        return mKeyguardStatusView;
-    }
-
     private void setStatusBar(StatusBar bar) {
         // TODO: this can be injected.
         mStatusBar = bar;
