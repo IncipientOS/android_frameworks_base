447302bde2c64ec26402bb3f67c48300b559ea2f
ROM-Jeremy <anthony.asher1986@gmail.com>
2021-07-26 20:34:18 -0400

    Revert "Port ambient Now Playing container from Pixel [stripped down version]"
    This reverts commit 426e9e4f753ffe9f8501070b41f4c8ec1d327de6.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -161,7 +161,6 @@ import com.android.systemui.Prefs;
 import com.android.systemui.R;
 import com.android.systemui.SystemUI;
 import com.android.systemui.SystemUIFactory;
-import com.android.systemui.ambientmusic.AmbientIndicationContainer;
 import com.android.systemui.assist.AssistManager;
 import com.android.systemui.biometrics.FODCircleViewImpl;
 import com.android.systemui.biometrics.FODCircleViewImplCallback;
@@ -1253,9 +1252,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         mAmbientIndicationContainer = mNotificationShadeWindowView.findViewById(
                 R.id.ambient_indication_container);
-        if (mAmbientIndicationContainer != null) {
-            ((AmbientIndicationContainer) mAmbientIndicationContainer).initializeView(this);
-        }
 
         mAutoHideController.setStatusBar(new AutoHideUiElement() {
             @Override
